4c5a2e71756aac70e67679ca25b31dc2fcb5aeec
david <dizozoranga1@tupl.edu.ec>
2024-05-01 16:35:55 -0500

    comentarios

---
+++ ejemplos-clases-constructores-diagramas/ejemplo0/codigo/ManejoClases/src/paquete3/Principal.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete3;
+
+/**
+ *
+ * @author User
+ */
+public class Principal {
+    public static void main(String[] args) {
+        // en esta clase de tipo main la clase computadora esta usando el constructor por defecto 
+        
+        Computadora computadoratrabajo = new Computadora();
+        
+        
+        
+    }
+}
